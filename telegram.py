7efc686616896f0f2165d81eeec2d1b57ab64abc
AntonPopov90 <mikprom12@>
2022-06-01 10:40:22 +0300

    30-05

--- telegram.py
+++ telegram.py
@@ -3,6 +3,7 @@ import psycopg2
 from datetime import date
 today = date.today()
 past_orders = []
+dates = []
 bot = telebot.TeleBot("5445102483:AAF8ohdfD941VIMUNG7_IOLRJzJjaepkymE")
 
 
@@ -12,14 +13,13 @@ connection = psycopg2.connect(user="postgres",
                               port="5432",
                               database="test_db")
 
-
 cursor = connection.cursor()
-postgreSQL_select_Query = "SELECT * FROM canal"
+postgreSQL_select_Query = "select order_number,delivery_time from canal"
 cursor.execute(postgreSQL_select_Query)
 canal_records = cursor.fetchall()
 for row in canal_records:
-    if row[3] < today:
-        past_orders.append(row[1])
+    if row[1] < today:
+        past_orders.append(row[0])
 
 
 @bot.message_handler(commands=['start'])
