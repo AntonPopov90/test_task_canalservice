6c747a12558e853c0afc6cf5ee6da793d99dd7b2
AntonPopov90 <mikprom12@>
2022-06-01 10:00:07 +0300

    30-05

--- canalservice_django/test_page/views.py
+++ canalservice_django/test_page/views.py
@@ -2,8 +2,11 @@ import json
 import psycopg2
 from datetime import datetime, date
 from django.shortcuts import render
+number = []
+orders_number = []
+price = []
 dates = []
-values = []
+sum_in_rub = []
 sum_of_orders = 0
 connection = psycopg2.connect(user="postgres",
                               password="123456",
@@ -13,23 +16,32 @@ connection = psycopg2.connect(user="postgres",
 
 
 cursor = connection.cursor()
-postgreSQL_select_Query = "SELECT price,delivery_time FROM canal"
+postgreSQL_select_Query = "SELECT * FROM canal"
 cursor.execute(postgreSQL_select_Query)
 canal_records = cursor.fetchall()
 for row in canal_records:
-    values.append(row[0])
-    dates.append(row[1])
-total_sum = sum(values)
+    number.append(row[0])
+    orders_number.append(row[1])
+    price.append(row[2])
+    dates.append(row[3])
+    sum_in_rub.append(row[4])
+total_sum = sum(price)
+
 
 def serialize_data(object):
     ''' Serialize dete format into JSON '''
     if isinstance(object, (datetime, date)):
         serial = object.isoformat()
         return serial
-values_to_json = {'values': values}
+values_to_json = {'values': price}
 dates_to_json = {'date': dates}
-values = json.dumps(values_to_json, default=serialize_data)
-dates = json.dumps(dates_to_json, default=serialize_data)
+orders_price = json.dumps(values_to_json, default=serialize_data)
+orders_dates = json.dumps(dates_to_json, default=serialize_data)
+
 def home(request):
-    return render(request, 'base.html', context={'values': values, 'dates': dates, 'total':total_sum})
+    return render(request, 'base.html', context={'orders_price':orders_price,'price': price,
+                                                 'orders_dates': orders_dates, 'orders_number': orders_number,
+                                                  'number': number,'dates':dates,
+                                                 'total': total_sum
+                                                 })
 
--- launching_script.py
+++ launching_script.py
@@ -7,6 +7,7 @@ schedule.every(5).minutes.do(get_values)
 schedule.every(7).minutes.do(bulk_insert)
 schedule.every(5).minutes.do(send_expired_orders)
 
+
 while True:
     schedule.run_pending()
     time.sleep(5)
